dbaddc0288ea3a8abbc69853b0cb20a4b1b5b4e8
Akshay Kumar~ <akshaykumar261@gmail.com>
2026-04-15 17:07:36 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import http from "http";
 import { WebSocketServer } from "ws";
-
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
@@ -17,14 +16,31 @@ wss.on("connection", (ws) => {
   console.log(`User ${userId} Connected`);
   ws.on("message", (data) => {
     const parsed = JSON.parse(data);
-    const { to, message } = parsed;
-    console.log(`User ${userId} -> User ${to}: ${message}`);
-    //target user find karo
-    const targetUser = users.get(to);
-    if (targetUser && targetUser.readyState === 1) {
-      targetUser.send(`Private from ${userId}: ${message}`);
-    } else {
-      ws.send("User not available");
+    const { userKey, message, type } = parsed;
+    // single user chat
+    if (type === "private") {
+      console.log(`User ${userId} -> User ${userKey}: ${message}`);
+      //target user find karo
+      const targetUser = users.get(userKey);
+      if (targetUser && targetUser.readyState === 1) {
+        // receiver ko msg milaga
+        targetUser.send(
+          `Message received recived from user ${userId}: ${message}`,
+        );
+        // sender ko success message bhejo
+        ws.send(`Message sent successfully to user ${userKey}`);
+      } else {
+        ws.send(`User ${userKey} not available`);
+      }
+    }
+    if (type === "group") {
+      console.log(`Group message from user ${userId}: ${message}`);
+      users.forEach((client, id) => {
+        if (id !== userId && client.readyState === 1) {
+          client.send(`Group message recived from user ${userId}:${message}`);
+        }
+      });
+      ws.send("Group message sent successfully");
     }
   });
   ws.on("close", () => {
